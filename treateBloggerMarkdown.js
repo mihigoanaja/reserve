209f7c1318d1e0b2198b7696e31534bbfb79ecfa
Mihigo ER Anaja <59900663+mihigoanaja@users.noreply.github.com>
2024-12-17 07:40:32 +0200

    Update treateBloggerMarkdown.js

--- treateBloggerMarkdown.js
+++ treateBloggerMarkdown.js
@@ -5,4 +5,7 @@ var html = marker(text);
 
 // Display the HTML
 var output = document.getElementById("post-body")||document.getElementsByClassName("post-body")[0];
-output.innerHTML = html.replace("&amp;gt;",">");
+output.innerHTML = html.replace(/&amp;gt;/g,">");
+if (btnInPre!==undefined) {
+  btnInPre();
+}
