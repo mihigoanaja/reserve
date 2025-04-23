9239a157b7ea6ac3a19e5ad8d44d6d297430f13a
Mihigo ER Anaja <59900663+mihigoanaja@users.noreply.github.com>
2025-04-23 14:30:00 +0200

    Create pullAtinasResults-V2.js

---
+++ pullAtinasResults-V2.js
@@ -0,0 +1,25 @@
+Array.from(document.getElementsByClassName('atinas-results')).forEach((resultsContainer)=>{
+	let r = resultsContainer;
+	let url = ['https://atinas.alreflections.net/search/api.php?dq=', r.dataset.query, '&type=', r.dataset.search].join("");
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			resultsContainer.innerHTML = '';
+			let resultCount = 0;
+			data.forEach(result => {
+				resultCount++;
+				const resultHTML = `
+					<div class='result'>
+						<img src='${result.image}' style='${(r.dataset.showImage=="yes")||"display:none;"}'/>
+						<h2>${result.title}</h2>
+						<p class='description'>${result.description}</p>
+						<a class='button' href='${result.link}' target='_blank'>Read More</a>
+					</div>
+				`;
+				if ((resultsContainer.dataset.limit == undefined) || (resultsContainer.dataset.limit >= resultCount)) {
+					resultsContainer.innerHTML += resultHTML;
+				}
+			});
+		})
+		.catch(error => console.error('Error:', error));
+});
