1643618af125819d125783f240dd60f665816909
Mihigo ER Anaja <59900663+mihigoanaja@users.noreply.github.com>
2025-11-05 18:07:56 +0200

    Add CSS resource for styleYepperAdverts

--- treateAlreflectionsBlogger.js
+++ treateAlreflectionsBlogger.js
@@ -8,7 +8,8 @@
         { type: 'css', url: 'https://showcaseitchio.pages.dev/showcaseItchio.css' },
         { type: 'js', url: 'https://showcaseitchio.pages.dev/showcaseItchio.js' },
         { type: 'js', url: 'https://onetag.pages.dev/xpopup.js' },
-        { type: 'js', url: 'https://onetag.pages.dev/alreflections-poa-buttons-V2.js' }
+        { type: 'js', url: 'https://onetag.pages.dev/alreflections-poa-buttons-V2.js' },
+        { type: 'css', url: 'https://reserve-cc2.pages.dev/css/styleYepperAdverts-V2.css' }
     ];
 
     // Loop through the resources and add them to the document
