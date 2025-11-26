b6233ed14fa1ee3e2b818e780400a9cef862d710
Mihigo ER Anaja <59900663+mihigoanaja@users.noreply.github.com>
2025-11-26 16:32:09 +0200

    Add pullAtinasResults-V3.js for fetching results

---
+++ pullAtinasResults-V3.js
@@ -0,0 +1,28 @@
+Array.from(document.getElementsByClassName('atinas-results')).forEach((resultsContainer)=>{
+	let r = resultsContainer;
+	let url = ['https://apiproxy.alreflections.net/atinas/search?dq=', r.dataset.query, '&type=', r.dataset.search].join("");
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			resultsContainer.innerHTML = '';
+			let resultCount = 0;
+			if (resultsContainer.classList.contains('reverse')) {
+				data.reverse();
+			}
+			data.forEach(result => {
+				resultCount++;
+				const resultHTML = `
+					<div class='result'>
+						<img src='${result.image}' style='${(r.dataset.showImage=="yes")||"display:none;"}'/>
+						<h2>${result.title}</h2>
+						<p class='description'>${result.description}</p>
+						<a class='button' href='${result.link}' target='_blank'>Read More</a>
+					</div>
+				`;
+				if ((resultsContainer.dataset.limit == undefined) || (resultsContainer.dataset.limit >= resultCount)) {
+					resultsContainer.innerHTML += resultHTML;
+				}
+			});
+		})
+		.catch(error => console.error('Error:', error));
+});
