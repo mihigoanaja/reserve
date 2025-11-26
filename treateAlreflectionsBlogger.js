f4bb13333faf135e6ca977e849182fe4744b731b
Mihigo ER Anaja <59900663+mihigoanaja@users.noreply.github.com>
2025-11-26 16:37:58 +0200

    Update alreflections-poa-buttons URL to V3

--- treateAlreflectionsBlogger.js
+++ treateAlreflectionsBlogger.js
@@ -8,7 +8,7 @@
         { type: 'css', url: 'https://showcaseitchio.pages.dev/showcaseItchio.css' },
         { type: 'js', url: 'https://showcaseitchio.pages.dev/showcaseItchio-V2.js' },
         { type: 'js', url: 'https://onetag.pages.dev/xpopup.js' },
-        { type: 'js', url: 'https://onetag.pages.dev/alreflections-poa-buttons-V2.js' },
+        { type: 'js', url: 'https://onetag.pages.dev/alreflections-poa-buttons-V3.js' },
         { type: 'css', url: 'https://reserve-cc2.pages.dev/css/styleYepperAdverts-V2.css' }
     ];
 
