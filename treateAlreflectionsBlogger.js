73a2d051027d97ee4322197adeb1dea9020e24df
Mihigo ER Anaja <59900663+mihigoanaja@users.noreply.github.com>
2025-11-26 12:08:14 +0200

    Update showcaseItchio.js URL to version 2

--- treateAlreflectionsBlogger.js
+++ treateAlreflectionsBlogger.js
@@ -6,7 +6,7 @@
         { type: 'css', url: 'https://reserve-cc2.pages.dev/pullAtinasResults-V2.css' },
         { type: 'js', url: 'https://reserve-cc2.pages.dev/pullAtinasResults-V2.js' },
         { type: 'css', url: 'https://showcaseitchio.pages.dev/showcaseItchio.css' },
-        { type: 'js', url: 'https://showcaseitchio.pages.dev/showcaseItchio.js' },
+        { type: 'js', url: 'https://showcaseitchio.pages.dev/showcaseItchio-V2.js' },
         { type: 'js', url: 'https://onetag.pages.dev/xpopup.js' },
         { type: 'js', url: 'https://onetag.pages.dev/alreflections-poa-buttons-V2.js' },
         { type: 'css', url: 'https://reserve-cc2.pages.dev/css/styleYepperAdverts-V2.css' }
