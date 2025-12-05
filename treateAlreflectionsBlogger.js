a0425dcef8411feacce9a9e17606f9ec0e56885e
Mihigo ER Anaja <59900663+mihigoanaja@users.noreply.github.com>
2025-12-05 14:26:17 +0200

    Update treateAlreflectionsBlogger.js

--- treateAlreflectionsBlogger.js
+++ treateAlreflectionsBlogger.js
@@ -10,7 +10,8 @@
         { type: 'js', url: 'https://onetag.pages.dev/xpopup.js' },
         { type: 'js', url: 'https://onetag.pages.dev/alreflections-poa-buttons-V3.js' },
         { type: 'css', url: 'https://reserve-cc2.pages.dev/css/styleYepperAdverts-V2.css' },
-        { type: 'js', url: 'https://onetag.pages.dev/xAnchor.js' }
+        { type: 'js', url: 'https://onetag.pages.dev/xAnchor.js' },
+        { type: 'js', url: 'https://onetag.pages.dev/xBrendIn.js' }
     ];
 
     // Loop through the resources and add them to the document
