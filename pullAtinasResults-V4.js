220e7fbdc3362d5a672cc955265911888ffeb022
Mihigo ER Anaja <59900663+mihigoanaja@users.noreply.github.com>
2026-01-14 06:09:53 +0200

    Create pullAtinasResults-V4.js

---
+++ pullAtinasResults-V4.js
@@ -0,0 +1,34 @@
+Array.from(document.getElementsByClassName('atinas-results')).forEach(async (resultsContainer)=>{
+	let r = resultsContainer;
+    let renderer=await fetch("https://reserve.alreflections.net/json/atinas-apis.json");
+    let apijson=await renderer.json();
+    let apiurl=apijson[parseInt(Math.random()*(apijson.length))];
+	let url = [apiurl,'?dq=', r.dataset.query, '&type=', r.dataset.search].join("");
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			resultsContainer.innerHTML = '';
+			let resultCount = 0;
+			if (resultsContainer.classList.contains('reverse')) {
+				data.reverse();
+			}
+			data.forEach(result => {
+				resultCount++;
+				const resultHTML = `
+					<div class='result'>
+						<img src='${result.image}' style='${(r.dataset.showImage=="yes")||"display:none;"}'/>
+						<h2>${result.title}</h2>
+						<p class='description'>${result.description}</p>
+						<a class='button' href='${result.link}' target='_blank'>Read More</a>
+					</div>
+				`;
+				if ((resultsContainer.dataset.limit == undefined) || (resultsContainer.dataset.limit >= resultCount)) {
+					resultsContainer.innerHTML += resultHTML;
+				}
+			});
+            if (resultsContainer.innerHTML!='') {
+                resultsContainer.innerHTML+='<a href="https://atinas.alreflections.net" class="pba-btn"><i><b>Powered By Atinas</b> - Alreflections&#39; Search Engine</i></a>';
+            }
+		})
+		.catch(error => console.error('Error:', error));
+});
